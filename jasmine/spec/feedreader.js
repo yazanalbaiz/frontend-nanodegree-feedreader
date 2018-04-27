0899e59e9b7cb85f0c7d1363baeb9be0ad76e1f3
Yazan AL-Baiz <yazobeats@gmail.com>
2018-04-27 15:36:17 +0300

    added: Last two suites wireframe
    title

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -101,21 +101,25 @@ $(function() {
             icon.click();
             expect(document.body.classList.contains('menu-hidden')).toBe(true);
         });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', () => {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+    });
+        
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe('New Feed Selection', () => {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
     });
+    
 }());
