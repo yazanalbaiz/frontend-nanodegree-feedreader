6f4c1bf2bd2f33b63a6bf2fb7507b1863e2dd9b0
Yazan AL-Baiz <yazobeats@gmail.com>
2018-04-27 13:07:00 +0300

    added : a new test suite named "The menu"
    Implemented a new test suite named "The menu".

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -69,7 +69,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    define('The menu', () => {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
@@ -97,4 +97,5 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
